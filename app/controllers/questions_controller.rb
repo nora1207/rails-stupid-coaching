7c7c3231f75b49ef23b7207b4fe4fdb60a116e3a
Nora Naneche <nora.naneche@gmail.com>
2024-11-11 14:51:26 +0100

    Je n'ai pas terminé la partie front, je vais avancer sur l'exercice 3

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,25 @@
 class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+
+  def answer
+    question = params[:question]
+    @coachanswer = ['Great', 'Silly question, get dressed and go to work', 'I don t care get dressed and go to work']
+    if question == "I am going to work"
+      @coachanswer = "Great"
+    elsif question.end_with?('?')
+      @coachanswer = "Silly question, get dressed and go to work"
+    else
+      @coachanswer = "I don't care, get dressed and go to work"
+    end
+  end
 end
+
+ #@questions = @questions.select {|question| question == "I am going to work"; @coachanswer = "Great"}
+      #if params [:question]
+        #@questions = @questions.select {|question| question.include?("?"); @coachanswer = "Silly question, get dressed and go to work"}
+      #else
+
+#array de answer
